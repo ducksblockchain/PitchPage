c09c95d870f3f4e9ba94fc6451108c967f7e0446
DhruPtel <Dhrupat3l@gmail.com>
2022-03-14 17:35:24 -0700

    Updates

---
+++ precache-manifest.303a32493a1f99b1ef57322851df406d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "910b7c50b709ad6aecbf",
+    "url": "/PitchPage/static/css/main.38283220.chunk.css"
+  },
+  {
+    "revision": "910b7c50b709ad6aecbf",
+    "url": "/PitchPage/static/js/main.910b7c50.chunk.js"
+  },
+  {
+    "revision": "52257f38f2aa7699d6fd",
+    "url": "/PitchPage/static/css/1.00e1ef5f.chunk.css"
+  },
+  {
+    "revision": "52257f38f2aa7699d6fd",
+    "url": "/PitchPage/static/js/1.52257f38.chunk.js"
+  },
+  {
+    "revision": "aec0e120fcf2c33b333c",
+    "url": "/PitchPage/static/js/runtime~main.aec0e120.js"
+  },
+  {
+    "revision": "7934b5b6b3def6c9f4002999951cb868",
+    "url": "/PitchPage/static/media/logo.7934b5b6.png"
+  },
+  {
+    "revision": "8ebb603c4f75d0ded664b5d0945f1970",
+    "url": "/PitchPage/index.html"
+  }
+];
